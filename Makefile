434196e677185fd9f1a8d6bf90dbf85158e00550
Oscar Villavicencio <odvcencio@gmail.com>
2018-10-29 16:35:39 -0400

    some edits to seed db and make sure all connected

--- Makefile
+++ Makefile
@@ -6,9 +6,10 @@ new-binary:
 
 #Use this after code push only
 docker-build-api:
+	@export CURRENT_HEAD=$(git rev-parse HEAD)
+	@echo $${CURRENT_HEAD}
 	#only have to login (the below command) once per 12 hours
-	#@eval `aws ecr get-login --region us-east-1 --no-include-email`
-	@export CURRENT_HEAD=${git rev-parse HEAD}
+	@eval `aws ecr get-login --region us-east-1 --no-include-email`
 	@docker build -t mphclub_api -f ./docker/go-app-develop/Dockerfile .
 	@docker tag mphclub_api:latest 077003688714.dkr.ecr.us-east-1.amazonaws.com/mphclub_api:latest
 	@docker tag mphclub_api:latest 077003688714.dkr.ecr.us-east-1.amazonaws.com/mphclub_api:$${CURRENT_HEAD}
--- database/seed_db.go
+++ database/seed_db.go
@@ -2,6 +2,9 @@ package database
 
 import (
 	"fmt"
+	"log"
+
+	"mphclub-rest-server/models"
 
 	"github.com/go-pg/pg"
 )
@@ -13,5 +16,44 @@ func seedDB() {
 }
 
 func seedVehicles(db *pg.DB) {
-	fmt.Println("seeded")
+	var vehicles []*models.Vehicle
+	err := db.Model(&vehicles).Select()
+	if err != nil {
+		log.Println("something went wrong: ", err)
+	}
+	fmt.Println(vehicles)
+
+	if len(vehicles) == 0 {
+
+		someVehicle := &models.Vehicle{
+			Color:       "yellow",
+			DayMax:      10,
+			DayMin:      1,
+			Description: "This is a car",
+			Make:        "chevy",
+			Model:       "cavalier",
+			Seats:       3,
+			Status:      "not reserved",
+			Trim:        "tan",
+			VehicleType: "sedan",
+			Year:        2008,
+		}
+
+		for i := 0; i < 31; i++ {
+			vehicles = append(vehicles, someVehicle)
+		}
+
+		for i, vehicle := range vehicles {
+			var idString = fmt.Sprintf("%.3d", i)
+			vehicle.ID = idString
+
+			err := db.Insert(vehicle)
+			if err != nil {
+				log.Println("error!")
+				log.Println(err)
+			}
+		}
+	} else {
+		fmt.Println("already seeded")
+	}
 }
