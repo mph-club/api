9d6c45ae2c1ac141a1387c46bd6978cdf7a8bef8
Oscar Villavicencio <odvcencio@gmail.com>
2018-11-03 13:53:09 -0400

    everything working, good place to merge to master

--- api.go
+++ api.go
@@ -1,49 +1,12 @@
 package main
 
 import (
-	"log"
 	"mphclub-rest-server/database"
 	"mphclub-rest-server/server"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
 )
 
 func main() {
-	// initiateAuthorization()
 	database.CreateSchema()
 
 	server.CreateAndListen()
-}
-
-func initiateAuthorization() *string {
-	sess := session.Must(session.NewSession(&aws.Config{
-		Region: aws.String("us-east-1"),
-	}))
-
-	svc := cognitoidentityprovider.New(sess)
-
-	initAuth, err := svc.InitiateAuth(&cognitoidentityprovider.InitiateAuthInput{
-		ClientId: aws.String("61lmh73b4k5fdogsbhebmmld33"),
-		AuthFlow: aws.String("USER_PASSWORD_AUTH"),
-		AuthParameters: map[string]*string{
-			"USERNAME": aws.String("oscar@mphclub.com"),
-			"PASSWORD": aws.String("@Test123"),
-		},
-	})
-
-	if err != nil {
-		log.Println("fail!")
-		log.Println(err)
-	} else {
-		log.Println("success!")
-		log.Println(initAuth)
-
-		token := initAuth.AuthenticationResult.AccessToken
-
-		return token
-	}
-
-	return aws.String("attempt to auth failed")
 }
