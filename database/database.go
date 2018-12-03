c030ae5194b6157211d92c0579c9405a2c5d1033
Oscar Villavicencio <odvcencio@gmail.com>
2018-12-03 15:52:22 -0500

    editing middleware to take from lambda functions

--- database/database.go
+++ database/database.go
@@ -56,6 +56,7 @@ func CreateSchema() {
 		&models.UserNote{},
 		&models.VehicleNote{},
 		&models.DriverLicense{},
+		&models.Staff{},
 		//models go here
 	} {
 		err := db.CreateTable(model, nil)
